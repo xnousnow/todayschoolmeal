f7afa58d068909cef5e254994e4783e65fa67690
xnousnow <xnou.snow@gmail.com>
2022-12-24 15:34:18 +0900

    :sparkles: 음식 정보 모달 구현

--- src/modals/MenuInfo.vue
+++ src/modals/MenuInfo.vue
@@ -1,22 +1,28 @@
 <script lang="ts">
   import {
     IonModal,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
     IonContent,
   } from '@ionic/vue'
   import { defineComponent } from 'vue'
+
+  import importedFilterData from '@/filterData'
 
   export default defineComponent({
     props: {
       index: Number,
     },
+    data() {
+      return {
+        menu: importedFilterData[this.index as number],
+      }
+    },
+    watch: {
+      index() {
+        this.menu = importedFilterData[this.index as number]
+      },
+    },
     components: {
       IonModal,
-      IonHeader,
-      IonToolbar,
-      IonTitle,
       IonContent,
     },
   })
@@ -26,16 +32,62 @@
   <ion-modal>
     <ion-header>
       <ion-toolbar>
-        <ion-title>MenuInfo</ion-title>
         <ion-buttons slot="secondary">
           <ion-button @click="$emit('close')">닫기</ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">
-      {{ index }}
+    <ion-content>
+      <div class="carousel">
+        <div v-for="image in menu.images" :key="image">
+          <img :src="image" />
+        </div>
+      </div>
+      <div class="info">
+        <h1>{{ menu.name }}</h1>
+        <p>{{ menu.description }}</p>
+      </div>
     </ion-content>
   </ion-modal>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .carousel {
+    display: flex;
+    flex-direction: row;
+    scroll-snap-type: x mandatory;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    background-color: var(--ion-color-step-300, #edeef0);
+    scroll-behavior: smooth;
+    align-items: center;
+  }
+  .carousel div {
+    scroll-snap-align: start;
+    flex-shrink: 0;
+    width: 100%;
+  }
+  .ios .carousel div {
+    height: calc(78vh - 100px);
+  }
+  .md .carousel div {
+    height: calc(90vh - 100px);
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    border-radius: 10px;
+  }
+  .info {
+    padding: 2px 20px 20px 20px;
+  }
+  h1 {
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 1.2;
+  }
+  p {
+    margin: 0;
+  }
+</style>
