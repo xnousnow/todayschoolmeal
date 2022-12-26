a204d8a523d29cf8afebfcade71eda6cb23a69c9
xnousnow <xnou.snow@gmail.com>
2022-12-26 13:30:25 +0900

    :lipstick: datepicker 모달 그림자 진하게 수정

--- src/views/MealPage.vue
+++ src/views/MealPage.vue
@@ -299,7 +299,7 @@
     border-radius: 14px;
   }
   ion-modal.md ion-datetime {
-    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
     border-radius: 0;
   }
   @media screen and (max-width: 350px) {
