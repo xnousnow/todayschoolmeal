ca02529920960abdbb396822879a5a17676ececc
xnousnow <xnou.snow@gmail.com>
2023-01-07 14:56:41 +0900

    :bug: 작은 화면에서 기능 소개 화면의 기능이 가려지는 버그 수정

--- src/modals/FeatureTutorial.vue
+++ src/modals/FeatureTutorial.vue
@@ -1,10 +1,5 @@
 <script lang="ts">
-  import {
-    IonModal,
-    IonContent,
-    IonList,
-    modalController,
-  } from '@ionic/vue'
+  import { IonModal, IonContent, IonList, modalController } from '@ionic/vue'
   import {
     restaurantOutline,
     restaurantSharp,
@@ -51,41 +46,48 @@
 <template>
   <ion-modal>
     <ion-content>
-      <h1>오늘뭐먹지 시작하기</h1>
-      <ion-list lines="none">
-        <FeatureItem
-          :iosicon="restaurantOutline"
-          :mdicon="restaurantSharp"
-          title="급식 보기"
-          description="선택한 날짜의 급식을 빠르고 쉽게 볼 수 있습니다."
-        />
-        <FeatureItem
-          :iosicon="nutritionOutline"
-          :mdicon="nutritionSharp"
-          title="재료 정보 보기"
-          description="급식에 들어가는 재료들의 사진과 효능을 보여주어 낯설어하지 않고 먹어볼 수 있습니다."
-        />
-        <FeatureItem
-          :iosicon="searchOutline"
-          :mdicon="searchSharp"
-          title="재료 정보 검색하기"
-          description="재료 정보를 검색하여 볼 수도 있습니다."
-        />
-        <FeatureItem
-          :iosicon="schoolOutline"
-          :mdicon="schoolSharp"
-          title="전국 학교 모두 지원"
-          description="전국의 모든 초등학교를 지원합니다. 정보는 자동으로 업데이트됩니다."
-        />
-      </ion-list>
-      <div class="close-button">
-        <ion-button expand="block" @click="close">계속</ion-button>
+      <div class="wrapper">
+        <h1>오늘뭐먹지 시작하기</h1>
+        <ion-list lines="none">
+          <FeatureItem
+            :iosicon="restaurantOutline"
+            :mdicon="restaurantSharp"
+            title="급식 보기"
+            description="선택한 날짜의 급식을 빠르고 쉽게 볼 수 있습니다."
+          />
+          <FeatureItem
+            :iosicon="nutritionOutline"
+            :mdicon="nutritionSharp"
+            title="재료 정보 보기"
+            description="급식에 들어가는 재료들의 사진과 효능을 보여주어 낯설어하지 않고 먹어볼 수 있습니다."
+          />
+          <FeatureItem
+            :iosicon="searchOutline"
+            :mdicon="searchSharp"
+            title="재료 정보 검색하기"
+            description="재료 정보를 검색하여 볼 수도 있습니다."
+          />
+          <FeatureItem
+            :iosicon="schoolOutline"
+            :mdicon="schoolSharp"
+            title="전국 학교 모두 지원"
+            description="전국의 모든 초등학교를 지원합니다. 정보는 자동으로 업데이트됩니다."
+          />
+        </ion-list>
+        <div class="close-button">
+          <ion-button expand="block" @click="close">계속</ion-button>
+        </div>
       </div>
     </ion-content>
   </ion-modal>
 </template>
 
 <style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
   h1 {
     padding: 20px 16px 6px;
     transform-origin: left center;
@@ -103,11 +105,14 @@
     margin-top: 60px;
   }
   ion-list {
-    margin: 20px 40px 40px 37px;
+    margin: 20px 40px 0 37px;
+    padding-bottom: 20px;
     background: transparent;
     display: flex;
     flex-direction: column;
     gap: 24px;
+    overflow: scroll;
+    flex-grow: 1;
   }
   .item {
     display: flex;
@@ -141,12 +146,9 @@
     line-height: 1.3;
   }
   .close-button {
-    position: fixed;
-    bottom: 0;
-    left: 0;
     width: 100%;
     padding: 20px 22px 60px;
-    background: var(--ion-background-color);
+    background: var(--ion-background-color, #fff);
   }
   ion-button.ios {
     --padding-top: 25px;
