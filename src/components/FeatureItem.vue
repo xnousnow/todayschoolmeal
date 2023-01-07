793e26207ff2db189bb5870819a54330c054f36e
xnousnow <xnou.snow@gmail.com>
2023-01-07 13:51:47 +0900

    :art: 기능 소개 화면의 기능을 컴포넌트로 분리

---
+++ src/components/FeatureItem.vue
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { IonIcon } from '@ionic/vue'
+
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    props: {
+      iosicon: String,
+      mdicon: String,
+      title: String,
+      description: String,
+    },
+    components: {
+      IonIcon,
+    },
+  })
+</script>
+
+<template>
+  <div class="item">
+    <div class="icon">
+      <ion-icon :ios="(iosicon as any)" :md="(mdicon as any)" color="primary"></ion-icon>
+    </div>
+    <div class="description">
+      <h2>{{ title }}</h2>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .item {
+    display: flex;
+    gap: 16px;
+    align-items: center;
+  }
+  .icon {
+    font-size: 35px;
+  }
+  ion-icon {
+    font-size: 35px;
+  }
+  .description {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 1px;
+  }
+  h2 {
+    font-size: 15px;
+    font-weight: 600;
+    margin: 0;
+    padding: 0;
+  }
+  p {
+    font-size: 15px;
+    margin: 0;
+    padding: 0;
+    color: var(--ion-color-medium-shade);
+    word-break: keep-all;
+    line-height: 1.3;
+  }
+</style>
--- src/modals/FeatureTutorial.vue
+++ src/modals/FeatureTutorial.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { IonModal, IonHeader, IonContent, IonList, IonIcon } from '@ionic/vue'
+  import { IonModal, IonHeader, IonContent, IonList } from '@ionic/vue'
   import {
     restaurantOutline,
     restaurantSharp,
@@ -12,6 +12,8 @@
   } from 'ionicons/icons'
 
   import { defineComponent } from 'vue'
+
+  import FeatureItem from '@/components/FeatureItem.vue'
 
   export default defineComponent({
     setup() {
@@ -36,7 +38,8 @@
       IonHeader,
       IonContent,
       IonList,
-      IonIcon,
+
+      FeatureItem,
     },
   })
 </script>
@@ -47,63 +50,30 @@
     <ion-content>
       <h1>오늘뭐먹지 시작하기</h1>
       <ion-list lines="none">
-        <div class="item">
-          <div class="icon">
-            <ion-icon
-              :ios="restaurantOutline"
-              :md="restaurantSharp"
-              color="primary"
-            ></ion-icon>
-          </div>
-          <div class="description">
-            <h2>급식 보기</h2>
-            <p>선택한 날짜의 급식을 빠르고 쉽게 볼 수 있습니다.</p>
-          </div>
-        </div>
-        <div class="item">
-          <div class="icon">
-            <ion-icon
-              :ios="nutritionOutline"
-              :md="nutritionSharp"
-              color="primary"
-            ></ion-icon>
-          </div>
-          <div class="description">
-            <h2>재료 정보 보기</h2>
-            <p>
-              급식에 들어가는 재료들의 사진과 효능을 보여주어 낯설어하지 않고
-              먹어볼 수 있습니다.
-            </p>
-          </div>
-        </div>
-        <div class="item">
-          <div class="icon">
-            <ion-icon
-              :ios="searchOutline"
-              :md="searchSharp"
-              color="primary"
-            ></ion-icon>
-          </div>
-          <div class="description">
-            <h2>재료 정보 검색하기</h2>
-            <p>급식에 들어가지 않더라도 검색하여 정보를 볼 수 있습니다.</p>
-          </div>
-        </div>
-        <div class="item">
-          <div class="icon">
-            <ion-icon
-              :ios="schoolOutline"
-              :md="schoolSharp"
-              color="primary"
-            ></ion-icon>
-          </div>
-          <div class="description">
-            <h2>전국 학교 모두 지원</h2>
-            <p>
-              전국의 모든 초등학교를 지원합니다. 정보는 자동으로 업데이트됩니다.
-            </p>
-          </div>
-        </div>
+        <FeatureItem
+          :iosicon="restaurantOutline"
+          :mdicon="restaurantSharp"
+          title="급식 보기"
+          description="선택한 날짜의 급식을 빠르고 쉽게 볼 수 있습니다."
+        />
+        <FeatureItem
+          :iosicon="nutritionOutline"
+          :mdicon="nutritionSharp"
+          title="재료 정보 보기"
+          description="급식에 들어가는 재료들의 사진과 효능을 보여주어 낯설어하지 않고 먹어볼 수 있습니다."
+        />
+        <FeatureItem
+          :iosicon="searchOutline"
+          :mdicon="searchSharp"
+          title="재료 정보 검색하기"
+          description="재료 정보를 검색하여 볼 수도 있습니다."
+        />
+        <FeatureItem
+          :iosicon="schoolOutline"
+          :mdicon="schoolSharp"
+          title="전국 학교 모두 지원"
+          description="전국의 모든 초등학교를 지원합니다. 정보는 자동으로 업데이트됩니다."
+        />
       </ion-list>
       <div class="close-button">
         <ion-button expand="block" @click="close">계속</ion-button>
